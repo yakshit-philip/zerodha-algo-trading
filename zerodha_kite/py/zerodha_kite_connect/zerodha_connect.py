364819f132cc0282d44512f116644418c3029947
Yakshit Philip <yakshit.philip@gmail.com>
2020-08-21 22:28:12 +0530

    - Completed Auto Login Functionality

--- zerodha_kite/py/zerodha_kite_connect/zerodha_connect.py
+++ zerodha_kite/py/zerodha_kite_connect/zerodha_connect.py
@@ -25,15 +25,22 @@ class ZerodhaConnect:
 
 	@property
 	def kc_client(self):
-		kite_client = KiteConnect(api_key=creds.API_KEY)
-		REQUEST_TOKEN = self.get_request_token(login_url=(kite_client.login_url()))
-		data = kite_client.generate_session(request_token=REQUEST_TOKEN, api_secret=creds.API_SECRET)
-		access_token = data.get("access_token")
-		if not access_token:
-			print("Error in retrieving access token")
-			return
-		kite_client.set_access_token(access_token)
-		return kite_client
+		try:
+			if not self.__kc_client:
+				kite_client = KiteConnect(api_key=creds.API_KEY)
+				request_token = self.get_request_token(login_url=(kite_client.login_url()))
+				data = kite_client.generate_session(request_token=request_token, api_secret=creds.API_SECRET)
+				access_token = data.get("access_token", None)
+				if not access_token:
+					print("Error in retrieving access token")
+					return self.__kc_client
+				kite_client.set_access_token(access_token)
+				self.__kc_client = kite_client
+				return self.__kc_client
+		except Exception as e:
+			print("Error in creating kite client.")
+			return None
+
 
 	def get_request_token(self, login_url):
 		try:
@@ -49,10 +56,11 @@ class ZerodhaConnect:
 			password = driver.find_element_by_xpath(consts.PASSWORD_XPATH)
 			username.send_keys(creds.KITE_USER_ID)
 			password.send_keys(decrypt(creds.PASSWORD_ENCRYPTED))
+			driver.find_element_by_xpath(consts.LOGIN_BUTTON_XPATH).click()
 			pin = driver.find_element_by_xpath(consts.PIN_XPATH)
 			pin.send_keys(decrypt(creds.PIN_ENCRYPTED))
 			driver.find_element_by_xpath(consts.GO_BUTTON_XPATH)
-			time.sleep(consts.WAIT_TIME)
+			time.sleep(consts.SLEEP_TIME)
 			request_token = driver.current_url.split('=')[1].split('&action')[0]
 			if request_token:
 				return request_token
